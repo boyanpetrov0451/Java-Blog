df1228acc4ba2823b9bf7da3858baeb2464935db
boyanpetrov0451 <boyanpetrov3@gmail.com>
2016-11-29 18:22:28 +0200

    Admin Functionality

---
+++ blog/src/main/java/softuniBlog/bindingModel/UserEditBindingModel.java
@@ -0,0 +1,17 @@
+package softuniBlog.bindingModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Boyan on 29.11.2016 г..
+ */
+public class UserEditBindingModel extends UserBindingModel {
+    private List<Integer> roles;
+
+    public UserEditBindingModel() { this.roles = new ArrayList<>(); }
+
+    public List<Integer> getRoles() { return roles; }
+
+    public void setRoles(List<Integer> roles) { this.roles = roles; }
+}
